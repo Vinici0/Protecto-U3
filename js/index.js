2a925f0c6690d8920f93332d149930ca123998cf
majito02 <mjparraga2@espe.edu.ec>
2022-01-18 00:42:46 -0500

    CAMBIOS-JQUERY

--- js/index.js
+++ js/index.js
@@ -1,22 +1,7 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script>
-posicionarMenu();
- 
-$(window).scroll(function() {    
-    posicionarMenu();
+$(".requisitos").mouseenter(function(){
+    $(".objetivos").slideDown(1000);
 });
- 
-function posicionarMenu() {
-    var altura_del_header = $('.header').outerHeight(true);
-    var altura_del_menu = $('.menu').outerHeight(true);
- 
-    if ($(window).scrollTop() >= altura_del_header){
-        $('.menu').addClass('fixed');
-        $('.wrapper').css('margin-top', (altura_del_menu) + 'px');
-    } else {
-        $('.menu').removeClass('fixed');
-        $('.wrapper').css('margin-top', '0');
-    }
-}
- 
-</script>
+
+$(".requisitos").mouseleave(function(){
+    $(".objetivos").slideUp();
+});
