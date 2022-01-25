3e009437b7ba2d1fb2cc2e432612094fbfafc985
kevinpaute <75482127+kevinpaute@users.noreply.github.com>
2022-01-24 22:13:38 -0500

    Commit Descargar JSON

---
+++ js/exportJSON.js
@@ -0,0 +1,143 @@
+/*The MIT License (MIT)
+
+Copyright (c) 2018 https://github.com/FuriosoJack/TableHTMLExport
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.*/
+
+(function($){
+
+
+
+    $.fn.extend({
+        tableHTMLExport: function(options) {
+
+            var defaults = {
+                separator: ',',
+                newline: '\r\n',
+                ignoreColumns: '',
+                ignoreRows: '',  
+                htmlContent: false,
+                consoleLog: false,
+                trimContent: true,
+                quoteFields: true,
+                
+            };
+            var options = $.extend(defaults, options);
+
+
+            function quote(text) {
+                return '"' + text.replace('"', '""') + '"';
+            }
+
+
+            function parseString(data){
+
+                if(defaults.htmlContent){
+                    content_data = data.html().trim();
+                }else{
+                    content_data = data.text().trim();
+                }
+                return content_data;
+            }
+
+            function download(filename, text) {
+                var element = document.createElement('a');
+                element.setAttribute('href', 'data:text;charset=utf-8,' + encodeURIComponent(text));
+                element.setAttribute('download', filename);
+
+                element.style.display = 'none';
+                document.body.appendChild(element);
+
+                element.click();
+
+                document.body.removeChild(element);
+            }
+
+            /**
+             * Convierte la tabla enviada a json
+             * @param el
+             * @returns {{header: *, data: Array}}
+             */
+            function toJson(el){
+
+                var jsonHeaderArray = [];
+                $(el).find('thead').find('tr').not(options.ignoreRows).each(function() {
+                    var tdData ="";
+                    var jsonArrayTd = [];
+
+                    $(this).find('th').not(options.ignoreColumns).each(function(index,data) {
+                        if ($(this).css('display') != 'none'){
+                            jsonArrayTd.push(parseString($(this)));
+                        }
+                    });
+                    jsonHeaderArray.push(jsonArrayTd);
+
+                });
+
+                var jsonArray = [];
+                $(el).find('tbody').find('tr').not(options.ignoreRows).each(function() {
+                    var tdData ="";
+                    var jsonArrayTd = [];
+
+                    $(this).find('td').not(options.ignoreColumns).each(function(index,data) {
+                        if ($(this).css('display') != 'none'){
+                            jsonArrayTd.push(parseString($(this)));
+                        }
+                    });
+                    jsonArray.push(jsonArrayTd);
+
+                });
+
+
+                return {header:jsonHeaderArray[0],data:jsonArray};
+            }
+
+            var el = this;
+            var dataMe;
+            if(options.type == 'json'){
+
+                var jsonExportArray = toJson(el);
+
+                if(defaults.consoleLog){
+                    console.log(JSON.stringify(jsonExportArray));
+                }
+                dataMe = JSON.stringify(jsonExportArray);
+
+                download(options.filename,dataMe)
+                /*
+                var base64data = "base64," + $.base64.encode(JSON.stringify(jsonExportArray));
+                window.open('data:application/json;filename=exportData;' + base64data);*/
+            }else if(options.type == 'pdf'){
+
+                var jsonExportArray = toJson(el);
+
+                if(defaults.consoleLog){
+                    console.log(jsonExportArray);
+                }
+
+                var doc = new jsPDF('p', 'pt');
+                doc.autoTable(jsonExportArray.header, jsonExportArray.data);
+                doc.save(options.filename);
+
+            }
+            return this;
+        }
+    });
+})(jQuery);
+
